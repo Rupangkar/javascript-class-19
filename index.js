36eefffd329c95f3ad5e43970a26a6d86ce7301d
Rupangkar majumder <145119687+Rupangkar@users.noreply.github.com>
2023-11-02 11:18:45 +0600

    y

---
+++ index.js
@@ -0,0 +1,172 @@
+// var product = 'pant';
+// product='shirt';
+// console.log(product);
+
+// Array
+// let number = [ 'Rupangkar','129','Mazbah','128']; 
+// number.push('rupa');
+// console.log(number)
+
+// let string = ['kalam','jamal','jakir','tomal'];
+// string.pop()
+// console.log(string);
+
+// var number = [ 120, 130 ,140, 150];
+// number.unshift(180);
+// console.log(number);
+
+// let number =[132,134,'152',146,134,1256];
+// // number.shift();
+// console.log(number.indexOf(2));
+
+
+// const numbers = ['rupangkar','dipu',123,'kalu'];
+
+// console.log(numbers.indexOf('kalu'));
+
+// Object
+
+// let student = {
+//     name: 'Rupangkar',
+//     id:479776,
+//     age:22,
+//     address:"motlobe uttor chondpur",
+//     choge:"black",
+// }
+// console.log(student);
+
+
+ 
+// function number(one,tow) {
+//     let totalNumber = one * tow;
+//     return totalNumber;
+// }
+//  let result =number(124,213);
+//  console.log(result);
+
+// function multifly(one,tow) {
+//     let totalmultifly = one / tow;
+//     return totalmultifly;
+// }
+// let result = multifly(100,10);
+// console.log(result);
+
+// // function inchToFeet(inch) {
+// //     let feetResult = inch / 12
+// //     return feetResult;
+// }
+// let output = inchToFeet(120)
+// console.log(output);
+  
+// Is It a leap year or not
+
+// feetToInch
+
+// function feetToInch(feet) {
+//     const feetResult = feet * 10
+//     return feetResult;
+// }
+// const output =feetToInch(12)
+// console.log(output);
+
+
+// function feetToInch(feet){
+//     let feetResult = feet * 30
+//     return feetResult;
+// }
+// let result = feetToInch(12)
+// console.log(result);
+
+
+// Leepyear
+
+// function isLeepYear(year) {
+//     if(year % 4 == 0) {
+//         console.log('This year is Leep year');
+//     }
+//     else {
+//         console.log('This year is not Leep year');
+//     }
+// }
+
+// isLeepYear(2024)
+
+// function isLeepYear(year){
+//     if(year % 4 == 0){
+//        return "This year is Leep year"
+//     }
+//     else{
+//         console.log('This year is not Leep year');
+//     }
+// }
+// isLeepYear(2027);
+
+// miles to kolometter
+// function milesToKilomitter(miles) {
+//     const kilomitter = miles * 1.60934
+//     return kilomitter;
+// }
+// const result =milesToKilomitter(40)
+// console.log(result);
+
+ 
+// kilomitter to miles
+/* function kilomitterToMiles(kilomitter) {
+    const miles = kilomitter / 0.621371
+    return parseInt(miles);
+}
+const result =kilomitterToMiles(100)
+console.log(result);
+
+function milesTokilomitter(miles) {
+    const kilomitter = miles * 1.60934
+    return parseFloat(kilomitter);
+}
+const result =milesTokilomitter(56)
+console.log(result);
+ */
+
+// Event number
+
+// function eventNumber(Event) {
+//     if(Event % 2 ==0){
+//         return "This is Event number"
+//     }
+//     else{
+//         return "This is not Event number"
+//     }
+
+    
+// }
+// const result =eventNumber(6)
+// console.log(result);
+
+
+// Odd
+
+/* function oddNumber(Odd) {
+    if(Odd % 3 != 0){
+        return "This is Odd number"
+    }
+    else{
+        return "This is not Odd number"
+    }
+    
+}
+const result =oddNumber(9)
+console.log(result); */
+
+/* function isEventNumber(event) {
+    if(event == 0){
+        return "This provide mor then 0"
+    }
+    else if(event % 2 == 0){
+        return "This number is Event"
+    }
+    else{
+        return "This number is not Event"
+    }
+      
+}
+const result =isEventNumber(7)
+console.log(result); */
